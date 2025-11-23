2e89ac7c1b6962154042a56e5aadd92552996f5d
Fernando SosaC <pruebar@casa.com>
2025-11-22 23:53:59 -0300

    algo mas de bash

--- bash/ej1_crea_usuarios.sh
+++ bash/ej1_crea_usuarios.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 #Codigos de error, se manejan como variables permitiondo que estos sean cambiados de ser nesesario de una manera sencilla
 error_formato=1
 error_arch=2
@@ -36,7 +37,6 @@ while getopts ":ic:" opcion; do
     esac
 done
 
-
 #getopts solo procesa opciones y sus argumentos, sin tomar en cuenta argumentos posicionales (argumentos sin - o --)
 #Esto puede generar error si los usuarios introducen argumentos extra
 #Esto se solucionara con el comando "shift" y la variable de "getopts" "OPTIND", que mueve los modificadores dado por el usario
@@ -49,3 +49,24 @@ if [ $# -ne 1 ]; then
 fi
 
 archivo_usuarios="$1"
+
+#Verifico que el archivo existe
+if [ ! -e "$archivo_usuarios" ]; then
+    echo "Error de uso, el archivo '$archivo_usuarios' no existe." >&2
+    exit $error_arch
+fi
+#Verifico que es un archivo
+if [ ! -f "$archivo_usuarios" ]; then
+    echo "Error de uso, '$archivo_usuarios' no es un archivo regular." >&2
+    exit $error_arch
+fi
+#Verifico que tiene permiso de lectura
+if [ ! -r "$archivo_usuarios" ]; then
+    echo "Error de uso, no tiene permisos de lectura sobre '$archivo_usuarios'." >&2
+    exit $error_perm
+fi
+
+#Proceso archivo línea a línea
+
+#Cuento usarios creados
+total_ok=0
